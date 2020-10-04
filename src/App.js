c827b0bd8208fce4310f26d96483e4f822d86c25
LeeEugene1 <uwm1004@gmail.com>
2020-10-04 17:43:48 +0900

    testestest

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import { GlobalStyle } from './globalStyles';
 //Home
 import Home from './pages/HomePage/Home'
-//Service
+//Services
 import Products from './pages/Products/Products'
 
 
